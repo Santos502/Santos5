613761447f40475ab31494416f524365e802b881
Santos502 <75867339+Santos502@users.noreply.github.com>
2020-12-13 17:58:54 +0700

    mengganti nama var

--- datetime.py
+++ datetime.py
@@ -4,7 +4,7 @@
 #konversi tanggal dan waktu sekarang kedalam format: tanggal bulan tahun jam:menit
 import datetime
 
-konversi1 = datetime.datetime.now()
-hariSekarang = konversi1.strftime('%d %B %Y %H:%M')
+konversi = datetime.datetime.now()
+hariSekarang = konversi.strftime('%d %B %Y %H:%M')
 
 print(hariSekarang)
