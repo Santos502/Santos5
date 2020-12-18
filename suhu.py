d406504fe174567389c11e8af956b20b9cad12cb
Santos502 <75867339+Santos502@users.noreply.github.com>
2020-12-18 13:37:17 +0700

    menambah pilihan reamur fahrenheit dan kelvin

--- suhu.py
+++ suhu.py
@@ -1,12 +1,6 @@
-#Muhamad Agung Santoso
-
-#memperbaiki program sederhana
+#revisi lagi
 
 print('\n Kalkulator Suhu \n')
-
-print('''1. Celcius ke Reamur
-2. Celcius ke Fahrenheit
-3. Celcius ke Kelvin''')
 
 def kembali():
     print('apakah anda ingin lanjut? y|t')
@@ -14,36 +8,67 @@ def kembali():
     milih = str(input('apakah anda ingin lanjut?: '))
     while keluar:
         if (milih == 'y'):
-            return pilihan()
+            print('Ya')
+            return konversi()
         elif (milih == 't'):
-            print('Terima Kasih')
+            print('Terima Kasih!')
             keluar = False
         else:
             print('Masukkan pilihan yang sesuai')
             return kembali()
 
-def pilihan():
-    pilih = int(input('Masukkan pilihan anda: '))
-    #Reamur
+def konversi():
+    print('1. Celcius')
+    print('2. Reamur')
+    print('3. Fahrenheit')
+    print('4. Kelvin')
+    pilih = int(input('Masukkan pilihan konversi suhu: '))
+    #Celcius
     if (pilih == 1):
         celcius = float(input('Masukkan celcius: '))
+        print('celcius = ', celcius)
         reamur = (4/5) * celcius
         print(celcius, 'celcius =', reamur, 'reamur')
-        return kembali()
-    #Fahrenheit
-    elif (pilih == 2):
-        celcius = float(input('Masukkan celcius: '))
         fahrenheit = (9/5) * celcius + 32
         print(celcius, 'celcius =', fahrenheit, 'fahrenheit')
-        return kembali()
-    #Kelvin
-    elif (pilih == 3):
-        celcius = float(input('Masukkan celcius: '))
         kelvin = celcius + 273
         print(celcius, 'celcius =', kelvin, 'kelvin')
         return kembali()
+    #Reamur
+    elif (pilih == 2):
+        reamur = float(input('Masukkan reamur: '))
+        print('reamur = ', reamur)
+        celcius = (5/4) * reamur
+        print(reamur, 'reamur =', celcius, 'celcius')
+        fahrenheit = (9/4) * reamur + 32
+        print(reamur, 'reamur =', fahrenheit, 'fahrenheit')
+        kelvin = (5/4) * reamur + 273
+        print(reamur, 'reamur =', kelvin, 'kelvin')
+        return kembali()
+    #Fahrenheit
+    elif (pilih == 3):
+        fahrenheit = float(input('Masukkan fahrenheit: '))
+        print('fahrenheit = ', fahrenheit)
+        celcius = (5/9) * (fahrenheit-32)
+        print(fahrenheit, 'fahrenheit =', celcius, 'celcius')
+        reamur = (4/9) * (fahrenheit-32)
+        print(fahrenheit, 'fahrenheit =', reamur, 'reamur')
+        kelvin = (5/9) * (fahrenheit-32) + 273
+        print(fahrenheit, 'fahrenheit =', kelvin, 'kelvin')
+        return kembali()
+    #Kelvin
+    elif (pilih == 4):
+        kelvin = float(input('Masukkan kelvin: '))
+        print('kelvin = ', kelvin)
+        celcius = kelvin - 273
+        print(kelvin, 'kelvin =', celcius, 'celcius')
+        reamur = (4/5) * (kelvin-273)
+        print(kelvin, 'kelvin =', reamur, 'reamur')
+        fahrenheit = (9/5) * (kelvin-273) + 32
+        print(kelvin, 'kelvin =', fahrenheit, 'fahrenheit')
+        return kembali()
     else:
-        print('Masukkan pilihan yang benar!')
-        return pilihan()
+        print('Masukkan pilihan yang sesuai!')
+        return konversi()
 
-pilihan()
+konversi()
