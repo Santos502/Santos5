562c6d2731bcce6b76cc36974892c3e64e405032
Santos502 <75867339+Santos502@users.noreply.github.com>
2020-12-18 13:34:16 +0700

    membuat program sederhana konversi suhu

---
+++ suhu.py
@@ -0,0 +1,21 @@
+#Muhamad Agung Santoso
+
+#program sederhana
+
+print ("\nKALKULATOR CELCIUS\n")
+
+#Celcius
+celcius = float (input ("Celcius ="))
+print ("Celcius =", celcius,"derajat")
+
+#Reamur
+reamur = (4/5) * celcius
+print ("Reamur =", reamur,"derajat")
+
+#Fahrenheit
+fahrenheit = (9/5) * celcius + 32
+print ("Fahrenheit =", fahrenheit, "derajat")
+
+#Kelvin
+kelvin = celcius + 273
+print ("Kelvin =", kelvin, "derajat")
